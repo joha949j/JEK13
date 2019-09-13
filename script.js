487ba52b6be284882e4877e480734e5185f2e290
Johan Lund Jansen <johanjansen@Johans-MacBook-Pro.local>
2019-09-13 12:48:29 +0200

    second sorterings knapper virker

--- script.js
+++ script.js
@@ -1,48 +1,62 @@
- let rejser;
- let filter = "alle";
+let rejser;
+let filter = "alle";
+let type = "alle";
 
- document.addEventListener("DOMContentLoaded", getJson);
+document.addEventListener("DOMContentLoaded", getJson);
 
- async function getJson() {
-     let jsonData = await fetch("https://spreadsheets.google.com/feeds/list/10-V6mdF7edqkvZKgdKcaGvsdOC_mudHxh8SOal_2iEs/od6/public/values?alt=json");
-     console.log("jsonData", jsonData);
-     rejser = await jsonData.json();
-     start();
-     addEventListenerToButtons();
- }
+async function getJson() {
+    let jsonData = await fetch("https://spreadsheets.google.com/feeds/list/10-V6mdF7edqkvZKgdKcaGvsdOC_mudHxh8SOal_2iEs/od6/public/values?alt=json");
+    console.log("jsonData", jsonData);
+    rejser = await jsonData.json();
+    start();
+    addEventListenerToButtons();
+}
 
- function start() {
-     const dest = document.querySelector("#liste");
-     const temp = document.querySelector("template");
-     dest.innerHTML = "";
+function start() {
+    const dest = document.querySelector("#liste");
+    const temp = document.querySelector("template");
+    dest.innerHTML = "";
 
-     rejser.feed.entry.forEach((rejse) => {
-         if (filter == "alle" || filter == rejse.gsx$verdensdel.$t) {
-             const klon = temp.cloneNode(true).content;
+    rejser.feed.entry.forEach((rejse) => {
+        if ((filter == "alle" || filter == rejse.gsx$verdensdel.$t) && (type == "alle" || type == rejse.gsx$kategori.$t)) {
+            const klon = temp.cloneNode(true).content;
 
-             klon.querySelector("img").src = `imgs/${rejse.gsx$billede.$t}.jpg`;
-             klon.querySelector("h2").textContent = rejse.gsx$navn.$t;
-             dest.appendChild(klon);
-             dest.lastElementChild.addEventListener("click", () => {
-                 location.href = `singleView.html?navn=${rejse.gsx$navn.$t}`;
-             });
-         }
-     })
- }
+            klon.querySelector("img").src = `imgs/${rejse.gsx$billede.$t}.jpg`;
+            klon.querySelector("h2").textContent = rejse.gsx$navn.$t;
+            dest.appendChild(klon);
+            dest.lastElementChild.addEventListener("click", () => {
+                location.href = `singleView.html?navn=${rejse.gsx$navn.$t}`;
+            });
+        }
+    })
+}
 
 
- function addEventListenerToButtons() {
-     document.querySelectorAll(".filter").forEach(elm => {
-         elm.addEventListener("click", filtrering);
-     })
- }
+function addEventListenerToButtons() {
+    document.querySelectorAll(".filter").forEach(elm => {
+        elm.addEventListener("click", filtrering);
+    })
+    document.querySelectorAll(".type").forEach(elm => {
+        elm.addEventListener("click", filtreringstype);
+    })
+}
 
- function filtrering() {
-     filter = this.dataset.kat;
-     document.querySelectorAll(".filter").forEach(elm => {
-         elm.classList.remove("nu");
+function filtrering() {
+    filter = this.dataset.kat;
+    document.querySelectorAll(".filter").forEach(elm => {
+        elm.classList.remove("nu");
 
-     })
-     this.classList.add("nu");
-     start();
- }
+    })
+    this.classList.add("nu");
+    start();
+}
+
+function filtreringstype() {
+    type = this.dataset.type;
+    document.querySelectorAll(".type").forEach(elm => {
+        elm.classList.remove("nu");
+
+    })
+    this.classList.add("nu");
+    start();
+}
